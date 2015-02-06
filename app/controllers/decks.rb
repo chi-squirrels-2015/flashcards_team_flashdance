3f6e837998b04d3f367aa98ac9a9e5179f487096
Andrew Irwin <andrew.irwin33@gmail.com>
2015-02-06 17:21:02 -0600

    commit files for viewer

--- app/controllers/decks.rb
+++ app/controllers/decks.rb
@@ -1,30 +1,30 @@
-# get '/homepage' do
-#   erb :'homepage'
+get '/homepage' do
+  erb :'homepage'
+end
+
+# get '/decks/:id' do
+
+#   erb :"/deck/show"
 # end
 
-get '/decks/:id' do
+# get '/decks/:id/cards/new' do
+# @deck = Deck.find(params[:id])
+# @deck.random_card
+#   #pull card from deck randomly
+#   #present card.definition in view
+#   erb :'decks/new'
+# end
 
-  erb :"/deck/show"
-end
+# post '/decks/:id/cards' do
 
-get '/decks/:id/cards/new' do
-@deck = Deck.find(params[:id])
-@deck.random_card
-  #pull card from deck randomly
-  #present card.definition in view
-  erb :'decks/new'
-end
-
-post '/decks/:id/cards' do
-
-  if correct,
-    mark as solved
-    erb :correct
-    if deck.solved = true
-      print erb :win
-  elsif wrong
-    mark as unsolved
-    erb :wrong
-end
+#   if correct,
+#     mark as solved
+#     erb :correct
+#     if deck.solved = true
+#       print erb :win
+#   elsif wrong
+#     mark as unsolved
+#     erb :wrong
+# end
 
 
